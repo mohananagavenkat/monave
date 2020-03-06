f32a3933f8619a80ce928bf2101c524e8b860adf
mohananagavenkat <mohananagavenkat@gmail.com>
2020-03-06 08:25:01 +0530

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyD-NvR9M3Y3r8mpxQ3u_4z4J3I6-8PAfL8",
+    authDomain: "angularapp-venkat.firebaseapp.com",
+    databaseURL: "https://angularapp-venkat.firebaseio.com",
+    projectId: "angularapp-venkat",
+    storageBucket: "angularapp-venkat.appspot.com",
+    messagingSenderId: "949773866996"
+  }
 };
