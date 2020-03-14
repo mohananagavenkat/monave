38b7c03436f7023608c9bf7c35ef4e5bfc673a0e
mohananagavenkat <mohananagavenkat@gmail.com>
2020-03-14 22:08:21 +0530

    cleaned up app component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component , OnInit, HostBinding } from '@angular/core';
-import { Title , Meta } from '@angular/platform-browser';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
@@ -10,32 +9,12 @@ import { Router, NavigationEnd } from '@angular/router';
 export class AppComponent implements OnInit {
   title = 'app';
 
-  public constructor(private titleService:Title , private metaService:Meta, private router:Router ){
+  public constructor(private router:Router ){
     this.routeEvent(this.router);
   }
 
   public ngOnInit(){
-    this.metaService.addTag({name:"",content:""});
-    this.metaService.addTag({name:"",content:""});
-    this.metaService.addTag({name:"",content:""});
-    this.metaService.addTag({name:"",content:""});
-    this.getAuthor();
-    this.getAllMeta();
  }
-
-  public setTitle(title:string){
-    this.titleService.setTitle(title);
-    this.title = this.titleService.getTitle();
-  }
-
-  getAuthor(){
-    console.log(this.metaService.getTag('name=author'));
-    console.log(this.metaService.getTag('name=author').getAttribute('content'));
-  }
-
-  getAllMeta(){
-    console.log(window.document.querySelectorAll('meta[name]'));
-  }
 
   routeEvent(router: Router){
     router.events.subscribe(e => {
